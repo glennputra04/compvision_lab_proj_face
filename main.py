2e028301fb3dee2084e1c1029ec58ac0060b960d
glennputra04 <glennputralaymando4@gmail.com>
2026-09-20 15:55:47 +0700

    Feat: Add Haar Cascade for setup

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ import cv2
 import numpy as np
 import os
 
+haar_path = os.path.join(
+    "haarcascade",
+    "haarcascade_frontalface_default.xml"
+)
+
+haar_cascade = cv2.CascadeClassifier(haar_path)
+
+if haar_cascade.empty():
+    print("Failed to load Haar Cascade")
+else:
+    print("Haar Cascade ready")
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
 def test() :
     print("Hello World")
